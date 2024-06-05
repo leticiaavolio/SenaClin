e7c731cfad273e4ea6d7c4a573c5758c40727559
leticia <leticia>
2024-06-05 20:32:07 -0300

    Exercício 11 - Selecionar a quantidade de dentistas pela especialidade

--- dml_dbSenaClin.sql
+++ dml_dbSenaClin.sql
@@ -86,6 +86,12 @@ SELECT * FROM dentista;
 SELECT dentista.nome, dataConsulta, tipoConsulta FROM dentista 
 LEFT JOIN consulta ON dentista.codDentista=consulta.codDentista;
 
+/*exercicio 11 -11.	Criar uma query que exiba a quantidade de dentistas que a clínica possui, agrupando-os por especialidade, ordenando pelo nome da especialidade de A-Z. Para isto, você utilizará o comando group by do sql. */
+
+SELECT * FROM dentista;
+
+SELECT COUNT(codDentista) AS 'Quant. Dentista', especialidade 
+FROM dentista GROUP BY especialidade ORDER BY especialidade ASC;
 
 
 
